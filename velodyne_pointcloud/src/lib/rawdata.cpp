aa9833b83560f25f5223f57725e71e577a35af8b
Thomas Emter <thomas.emter@iosb.fraunhofer.de>
2025-06-02 12:10:02 +0200

    Add point time stamp of packet if no table is available

--- velodyne_pointcloud/src/lib/rawdata.cpp
+++ velodyne_pointcloud/src/lib/rawdata.cpp
@@ -329,6 +329,8 @@ void RawData::unpack(
         if (timing_offsets_.size()) {
           time = timing_offsets_[i][j] + time_diff_start_to_this_packet;
         }
+        else
+          time = time_diff_start_to_this_packet;
 
         if (tmp.uint == 0) {  // no valid laser beam return
           // call to addPoint is still required since output could be organized
